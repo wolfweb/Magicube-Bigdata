3112c63d89867e48a40491a2de08c20979618b99
wolfweb <wolfweb@outlook.com>
2019-11-10 22:37:38 +0800

    optimize flink init

--- core/eventflows.core/src/main/scala/com/magicube/eventflows/core/FlinkConf.scala
+++ core/eventflows.core/src/main/scala/com/magicube/eventflows/core/FlinkConf.scala
@@ -3,4 +3,6 @@ package com.magicube.eventflows.core
 abstract class FlinkConf() {
   val interval: Long = 1500
   val parallelCount: Int = -1
+  val retry:Int = 3
+  val retryDelay = 1000
 }
--- core/eventflows.core/src/main/scala/com/magicube/eventflows/core/Input/InputComponent.scala
+++ core/eventflows.core/src/main/scala/com/magicube/eventflows/core/Input/InputComponent.scala
@@ -4,7 +4,8 @@ import com.magicube.eventflows.Component
 import com.magicube.eventflows.Exceptions.EventflowException
 import com.magicube.eventflows.core._
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
-import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.environment.CheckpointConfig.ExternalizedCheckpointCleanup
+import org.apache.flink.streaming.api.{CheckpointingMode, TimeCharacteristic}
 import org.apache.flink.streaming.api.scala._
 
 abstract class InputComponent() extends Component{
@@ -15,10 +16,16 @@ abstract class InputComponent() extends Component{
     val flinkConf = conf.asInstanceOf[FlinkConf]
     val _env = StreamExecutionEnvironment.getExecutionEnvironment
     _env.enableCheckpointing(flinkConf.interval)
-    _env.setRestartStrategy(RestartStrategies.noRestart())
+    env.setRestartStrategy(RestartStrategies.fixedDelayRestart(flinkConf.retry,flinkConf.retryDelay))
+    _env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+    env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
+    env.getCheckpointConfig.setMinPauseBetweenCheckpoints(500)
+    env.getCheckpointConfig.setCheckpointTimeout(60000)
+    env.getCheckpointConfig.setMaxConcurrentCheckpoints(1)
+    env.getCheckpointConfig.enableExternalizedCheckpoints(ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION)
+
     if (flinkConf.parallelCount > 0)
       _env.setParallelism(flinkConf.parallelCount)
-    _env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
     _env
   }
 
