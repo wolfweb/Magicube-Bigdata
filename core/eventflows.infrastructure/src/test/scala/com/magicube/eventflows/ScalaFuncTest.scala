cfd1dbe164c09cdcae223cd3027e535f32a6e5b2
changwei.zhao <changwei@we7.cn>
2019-11-25 09:46:26 +0800

    add test

--- core/eventflows.infrastructure/src/test/scala/com/magicube/eventflows/ScalaFuncTest.scala
+++ core/eventflows.infrastructure/src/test/scala/com/magicube/eventflows/ScalaFuncTest.scala
@@ -1,5 +1,36 @@
 package com.magicube.eventflows
 
-class ScalaFuncTest {
+import org.junit.Test
 
+class ScalaFuncTest{
+  @Test
+  def func_Demo_Test(): Unit = {
+    Speaker To Weather(Some(() => "today")) Do "hello"
+    Speaker To Human(Some(() => "lilei")) Do "nice to meet you"
+  }
+}
+
+
+object Speaker {
+  def To(el: => What): What = {
+    el
+  }
+
+  def weather(el: => String): What = {
+    Weather(Some(el _))
+  }
+}
+
+trait What {
+  def Do[R](el: => R) = {
+    println(el)
+  }
+}
+
+case class Weather(someDay: Option[() => String]) extends What {
+  print(s"${someDay.get.apply()} ")
+}
+
+case class Human(name: Option[() => String]) extends What {
+  print(s"${name.get.apply()} ")
 }
